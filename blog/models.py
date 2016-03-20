3d614e76f1a938628990fdab03f9d9a4f40a0622
Combo-Breaker <impecopeco@gmail.com>
2016-03-20 11:17:54 +0300

    +forms

--- blog/models.py
+++ blog/models.py
@@ -9,10 +9,19 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
-
+   
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+
+#class Board(models.Model):
+#    title = models.CharField(max_length=255)
+#    posts = models.Post()
+
+#    def get_absolute_url(self):
+#        return "/blog/temlates/blog/%i/" % self.id
+    
